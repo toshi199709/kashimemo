a7ced3d3a1f8cd8d3f531f45bed3ec21ccca7bc9
toshi199709 <ayatoshi077@gmail.com>
2025-05-29 15:35:37 +0900

    N1問題修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,10 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @posts = Post.all.order(created_at: :desc)
-  end
+def index
+  @posts = Post.includes(:user).order(created_at: :desc)
+end
+
 
   def new
     @post = Post.new
