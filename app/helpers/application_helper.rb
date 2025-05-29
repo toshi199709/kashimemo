d8aa5fc2506c17e35e86d0bd2a11e0b10c41627f
toshi199709 <ayatoshi077@gmail.com>
2025-05-29 21:39:22 +0900

    動画の読み込み設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  def youtube_embed_url(url)
+    return "" if url.blank?
+
+    uri = URI.parse(url)
+
+    video_id =
+      if uri.host.include?("youtu.be")
+        uri.path.delete_prefix("/")
+      elsif uri.host.include?("youtube.com")
+        params = Rack::Utils.parse_nested_query(uri.query)
+        params["v"]
+      end
+
+    "https://www.youtube.com/embed/#{video_id}" if video_id.present?
+  rescue URI::InvalidURIError
+    ""
+  end
 end
--- app/helpers/posts_helper.rb
+++
@@ -1,6 +0,0 @@
-module PostsHelper
-  def youtube_embed_url(url)
-    youtube_id = url.split("v=").last&.split("&")&.first
-    "https://www.youtube.com/embed/#{youtube_id}" if youtube_id.present?
-  end
-end
