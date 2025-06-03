e71539f855418b7823039c03a2d8169ee0e1462a
toshi199709 <ayatoshi077@gmail.com>
2025-06-04 07:19:24 +0900

    インポート修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "youtube_loader"
+pin "youtube_loader", to: "youtube_loader.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
