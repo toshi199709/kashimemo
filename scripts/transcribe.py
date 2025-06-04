096f24d80d67ffcf0cd34e88ebb195c8c257736d
toshi199709 <ayatoshi077@gmail.com>
2025-06-04 15:28:31 +0900

    テキスト書き出し修正

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -5,6 +5,10 @@ import sys
 
 model = whisper.load_model("medium")  # tiny や base でもOK。用途に応じて調整可
 audio_path = sys.argv[1]
-result = model.transcribe(audio_path, language="ja")
+result = model.transcribe(
+    audio_path,
+    language="ja",
+    temperature=0.2
+)
 
 print(result["text"])
