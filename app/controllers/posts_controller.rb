2a01f2ac355d2969305f6bb6a71934f8bd882e31
toshi199709 <ayatoshi077@gmail.com>
2025-06-06 11:40:08 +0900

    コントローラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,66 +3,69 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :update, :destroy]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
-def generate_lyrics
+
+  def generate_lyrics
   url = params[:video_url]
   lyrics = WhisperTranscriber.call(url)
   render json: { lyrics: lyrics }
 end
 
-def index
-  if user_signed_in?
-    @posts = Post.includes(:user).where("is_public = ? OR user_id = ?", true, current_user.id).order(created_at: :desc)
-  else
-    @posts = Post.includes(:user).where(is_public: true).order(created_at: :desc)
+
+  def index
+    if user_signed_in?
+      @posts = Post.includes(:user).where("is_public = ? OR user_id = ?", true, current_user.id).order(created_at: :desc)
+    else
+      @posts = Post.includes(:user).where(is_public: true).order(created_at: :desc)
+    end
   end
-end
-
-
 
   def new
     @post = Post.new
   end
 
-def create
-  @post = Post.new(post_params)
-  @post.user = current_user
-  if @post.save
-    redirect_to posts_path, notice: '投稿を保存しました！' # ← 一覧ページへリダイレクト
-  else
-    render :new
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      if @post.audio_file.attached?
+        lyrics = WhisperTranscriber.call(@post.audio_file.blob)
+        @post.update(lyrics: lyrics)
+      end
+
+      redirect_to posts_path, notice: '投稿を保存しました！'
+    else
+      render :new
+    end
   end
-end
 
-def edit
-  session[:return_to] = request.referer
-end
-
-def update
-  if @post.update(post_params)
-    redirect_to(session.delete(:return_to) || posts_path, notice: "編集が完了しました！")
-  else
-    render :edit
+  def edit
+    session[:return_to] = request.referer
   end
-end
 
-def destroy
-  @post.destroy
-  redirect_to(session.delete(:return_to) || posts_path, notice: "削除しました！")
-end
-
-
-def preview
-  @post = Post.find(params[:id])
-  unless @post.is_public || (user_signed_in? && current_user == @post.user)
-    redirect_to root_path, alert: "この投稿は非公開です"
+  def update
+    if @post.update(post_params)
+      redirect_to(session.delete(:return_to) || posts_path, notice: "編集が完了しました！")
+    else
+      render :edit
+    end
   end
-end
 
+  def destroy
+    @post.destroy
+    redirect_to(session.delete(:return_to) || posts_path, notice: "削除しました！")
+  end
 
-def mypage
-  @posts = current_user.posts.order(created_at: :asc) # 古い順に並べる
-end
+  def preview
+    @post = Post.find(params[:id])
+    unless @post.is_public || (user_signed_in? && current_user == @post.user)
+      redirect_to root_path, alert: "この投稿は非公開です"
+    end
+  end
 
+  def mypage
+    @posts = current_user.posts.order(created_at: :asc)
+  end
 
   private
 
@@ -71,12 +74,11 @@ end
   end
 
   def post_params
-    params.require(:post).permit(:title, :video_url, :lyrics, :memo, :is_public)
+    params.require(:post).permit(:title, :video_url, :audio_file, :lyrics, :memo, :is_public)
   end
 
   def authorize_user!
-  @post = Post.find(params[:id])
-  redirect_to root_path, alert: "不正なアクセスです" unless @post.user == current_user
+    @post = Post.find(params[:id])
+    redirect_to root_path, alert: "不正なアクセスです" unless @post.user == current_user
   end
 end
-
