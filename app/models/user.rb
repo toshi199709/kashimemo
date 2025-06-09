c7d011a8eb33ce08c596f3f492713d76218b9eee
toshi199709 <ayatoshi077@gmail.com>
2025-06-09 16:53:37 +0900

    モデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :nickname, presence: true
 
   validate :password_complexity
