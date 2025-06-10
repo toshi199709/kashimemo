783597474038aaec78d9c44dd9de7c2bb063d1d1
toshi199709 <ayatoshi077@gmail.com>
2025-06-10 15:04:04 +0900

    いいね機能のテスト

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @post = FactoryBot.create(:post)
+  end
+
+  context "ログインしている場合" do
+    before do
+      sign_in @user
+    end
+
+    it "投稿にいいねできる" do
+      expect do
+        post post_likes_path(@post)
+      end.to change(Like, :count).by(1)
+    end
+
+    it "投稿のいいねを解除できる" do
+      like = FactoryBot.create(:like, user: @user, post: @post)
+
+      expect do
+        delete post_like_path(@post, like)
+      end.to change(Like, :count).by(-1)
+    end
+  end
+
+  context "ログインしていない場合" do
+    it "いいねしようとするとログインページにリダイレクトされる" do
+      post post_likes_path(@post)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "いいね解除しようとするとログインページにリダイレクトされる" do
+      like = FactoryBot.create(:like, post: @post, user: @user)
+      delete post_like_path(@post, like)
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 end
