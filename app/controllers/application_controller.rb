ce58d258a8da6d65f87db178a7c183a4fe2dbff6
toshi199709 <ayatoshi077@gmail.com>
2025-06-11 08:48:39 +0900

    本番用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: -> { Rails.env.production? }
 
   protected
 
@@ -15,9 +15,5 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
-  end
-
-  def production?
-    Rails.env.production?
   end
 end
