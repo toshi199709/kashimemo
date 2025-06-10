91f52bc62e59fee6b000bd34e2e361d33744845b
toshi199709 <ayatoshi077@gmail.com>
2025-06-10 14:39:01 +0900

    Likeモデルのテスト

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :post, counter_cache: true
+  belongs_to :post
 
-  validates :user_id, uniqueness: { scope: :post_id }
+  validates :user_id, uniqueness: { scope: :post_id } # ← これが一意性制約
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  context '有効ないいねの場合' do
+    it 'userとpostが紐づいていれば有効' do
+      expect(@like).to be_valid
+    end
+  end
+
+  context '無効な場合' do
+    it 'userが紐づいていなければ無効' do
+      @like.user = nil
+      @like.valid?
+      expect(@like.errors.full_messages).to include("User must exist")
+    end
+
+    it 'postが紐づいていなければ無効' do
+      @like.post = nil
+      @like.valid?
+      expect(@like.errors.full_messages).to include("Post must exist")
+    end
+
+    it '同じユーザーが同じ投稿に2回いいねすることはできない' do
+      @like.save # 1回目のいいね
+      another_like = FactoryBot.build(:like, user: @like.user, post: @like.post)
+      another_like.valid?
+      expect(another_like.errors.full_messages).to include("User has already been taken")
+    end
+  end
 end
