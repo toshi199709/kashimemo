c1b504b77a6f02bd9ae677378d63d4a826e6e897
toshi199709 <ayatoshi077@gmail.com>
2025-06-17 11:52:02 +0900

    postモデルに追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   validates :video_url, presence: true
   validates :lyrics, presence: true
   validates :memo, presence: true
   validates :title, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "を選択してください" }
 end
