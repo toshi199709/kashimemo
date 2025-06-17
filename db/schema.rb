17883337cb48234d72453b5cf2817883ea394a2d
toshi199709 <ayatoshi077@gmail.com>
2025-06-17 12:05:02 +0900

    postsテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_09_090739) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_17_030344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_09_090739) do
     t.string "background_image_url"
     t.float "background_opacity"
     t.integer "likes_count", default: 0, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
