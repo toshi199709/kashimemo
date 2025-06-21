d2ecd6bc9a7dcc132688f9b960f8d20d5e0dad24
toshi199709 <ayatoshi077@gmail.com>
2025-06-22 08:26:02 +0900

    アソシエーション

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,5 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :playlist_items, dependent: :destroy
+  has_many :posts, through: :playlist_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :playlists, dependent: :destroy
 
   has_one_attached :background_image
 
